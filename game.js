2dcf9f56b4ee94ca7eedb1fbfb8c5f3b3b283582
Black Jesus <65342347+mahrusferdous@users.noreply.github.com>
2021-01-10 19:23:34 -0500

    Attempted fix
    Attempted to fix the app, but still issue with the if statement. Not sure how to approach the situation.

--- game.js
+++ game.js
@@ -1,78 +1,104 @@
+var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
-var buttonColors = ["red", "blue", "green", "yellow"];
-var level = 0;
 var started = false;
+var level = 0;
 
-//pushes into an array
-$(".btn").on("click", function(){
-  var userChosenColor = $(this).attr('id');
-  userClickedPattern.push(userChosenColor);
-  playSound(userChosenColor);
-  animatePress(userChosenColor);
-  checkAnswer(userClickedPattern.length);
-});
-
-$(document).keypress(function() {
-    if(!started) {
-      nextSequence();
-      started = true;
-    }
-});
-
-//selectes random colors and pushes into an array
-function nextSequence() {
-  var randomNumber = Math.round((Math.random())*3);
-  var randomChosenColor = buttonColors[randomNumber];
-  $("."+randomChosenColor).fadeOut(100).fadeIn(100);
-  playSound(randomChosenColor);
-  gamePattern.push(randomChosenColor);
-  level++;
-  $('h1').text("Level " + level);
-}
-
-//plays sounds
-function playSound(name) {
-  var audio = new Audio("sounds/" + name + ".mp3");
-  audio.play();
-}
-
-function animatePress(currentColor) {
-  var currentC = $("." + currentColor);
-  currentC.addClass("pressed");
-  setTimeout(function() {
-    currentC.removeClass("pressed");
-  }, 100);
-}
-
-function checkAnswer(currentLevel) {
-// THE IF STATEMENT DOES NOT WORK
-  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-    console.log("success");
-    if (userClickedPattern.length === gamePattern.length) {
-      setTimeout(function() {
+// Click anywhere on the screen to start
+$(document).keypress(function () {
+    if (!started) {
         nextSequence();
-      }, 1000);
+        started = true;
+        $("body").removeClass("game-over")
+    };
+});
+
+// Play sounds
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+// The random color
+function nextSequence() {
+    // Generates random color
+    var randomNumber = Math.round((Math.random()) * 3);
+    var randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+    level++;
+    $("h1").text("Level " + level);
+
+    // Makes color fade in and out
+    function start(counter) {
+        if (counter < gamePattern.length) {
+            setTimeout(function () {
+                $("." + gamePattern[counter++]).fadeOut(100).fadeIn(100);
+                start(counter)
+            }, 500)
+        }
     }
-  } else {
-    console.log("wrong");
-    gameOver();
-    startOver();
-  }
+    start(0);
+
+    function startS(counter) {
+        if (counter < gamePattern.length) {
+            setTimeout(function () {
+                playSound(gamePattern[counter++]);
+                startS(counter)
+            }, 500)
+        }
+    }
+    startS(0);
+    userClickedPattern.length = 0;
 }
 
+// My Color
+$(".btn").on("click", function () {
+    var userChosenColor = $(this).attr("id")
+    userClickedPattern.push(userChosenColor);
+    playSound(userChosenColor);
+    animateColor(userChosenColor);
+    checkAnswer(gamePattern.length)
+})
+
+// Individual color fade in fade out
+function animateColor(currentColor) {
+    var currentC = $("." + currentColor);
+    currentC.addClass("pressed");
+    setTimeout(function () {
+        currentC.removeClass("pressed");
+    }, 100);
+}
+
+//Game over
 function gameOver() {
-  $('h1').text("Game Over, Press Any Key to Restart")
-  var currentC = $("body");
-  currentC.addClass("game-over");
-  setTimeout(function() {
-    currentC.removeClass("game-over");
-  }, 200);
-  playSound("wrong");
+    $("h1").text("Game Over, Press Any Key To Restart");
+    $("body").addClass("game-over")
+    playSound("wrong");
 }
 
+//Start again
 function startOver() {
-  level = 0;
-  gamePattern.length = 0;
-  started = false;
+    userClickedPattern.length = 0;
+    gamePattern.length = 0;
+    started = false;
+    level = 0;
+}
+
+//Check answer
+//Not sure what's wrong because not running as intended
+function checkAnswer(currentLevel) {
+    for (var i = 0; i < currentLevel; i++) {
+        if (userClickedPattern[i] === gamePattern[i]) {
+            if (userClickedPattern.length === gamePattern.length) {
+                setTimeout(function () {
+                    nextSequence();
+                }, 1000);
+            }
+        } else {
+            console.log("wrong");
+            gameOver();
+            startOver();
+            break;
+        }
+    }
 }
